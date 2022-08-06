6d6be7d47bf6b9b11c565276baf46a35709cbca0
LizandroV <lizandrovivanco16@gmail.com>
2022-08-06 17:10:30 -0500

    Version 2.0

--- SIGE_Upddate/Borrar_test.py
+++
@@ -1,5 +0,0 @@
-document = docx.Document(filename)
-docText = '\n\n'.join([
-    paragraph.text.encode('utf-8') for paragraph in document.paragraphs
-])
-print docText
--- SIGE_Upddate/borrarSIGE.py
+++
@@ -1,7 +0,0 @@
-import os
-
-try:
-    os.remove("""C://Users//Public//Desktop//SIGE 2022.lnk""")
-    os.remove("""C://Users//Public//Desktop//Refresca Sistemas.lnk""")
-except OSError as e:
-    print(f"Error:{ e.strerror}")

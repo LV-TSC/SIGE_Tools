ab1da475b031a412f4ceefa01e97fcecc39260f2
LizandroV <lizandrovivanco16@gmail.com>
2022-08-06 17:14:56 -0500

    Version 2.0

---
+++ SIGE_Update/Test.py
@@ -0,0 +1,12 @@
+import os
+import time
+import glob
+import shutil
+from tkinter import messagebox
+import subprocess 
+
+subprocess.call(["""C:\\TSC\\SIGE\\D01OrdCorte.exe"""])
+subprocess.call(["""C:\\Program Files\\Mozilla Firefox\\firefox.exe"""]) 
+#Variables
+print("w")
+os.system("'start C:\\Program Files\\Mozilla Firefox\\firefox.exe'")
